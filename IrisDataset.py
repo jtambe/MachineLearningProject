6ce28559456c51c8b50a275dea07062c67da42fc
jtambe <tambe_jayesh@hotmail.com>
2017-02-17 20:23:42 -0800

    completed iris data example with pdf for visualization tree

--- IrisDataset.py
+++ IrisDataset.py
@@ -1,6 +1,9 @@
 
 # use a sample dataset from sklearn
 from sklearn.datasets import load_iris
+from sklearn import tree
+import numpy as np
+
 
 iris = load_iris()
 
@@ -21,6 +24,59 @@ print(iris.target_names)
 # printing out entire data and target
 # for i in range(len(iris.target)):
 #     print("Number: " + str(i) + " Data: " + str(iris.data[i]) + " Target: " + str(iris.target[i]))
+
+
+# setting up indices for testing data in loaded dataset
+testDataIndices = [0,50,100]
+
+# creating training data by removing data at those indices
+train_target = np.delete(iris.target, testDataIndices)
+# np.delete for list of lists
+#http://stackoverflow.com/questions/40348269/numpy-delete-list-element-from-list-of-lists
+train_data = np.delete(iris.data, testDataIndices, axis=0) # delete elements at indices at axis 0
+
+#print(train_data)
+# total_lists = (len(train_data)//4)
+# np.reshape(train_data, (4,-1) )
+
+#print(train_data)
+#print(train_target)
+# creating testing data by using the indices above
+test_target = iris.target[testDataIndices]
+test_data = iris.data[testDataIndices]
+
+
+classifier = tree.DecisionTreeClassifier()
+classifier.fit(train_data, train_target)
+
+
+print(test_target)
+print(classifier.predict(test_data))
+
+
+# sudo python3 -m pip install ipython
+# sudo python3 -m pip install pydot
+# sudo python3 -m pip install pydotplus
+# sudo apt-get install graphviz
+import pydotplus
+from IPython.display import Image
+dot_data = tree.export_graphviz(classifier, out_file=None,
+                         feature_names=iris.feature_names,
+                         class_names=iris.target_names,
+                         filled=True, rounded=True,
+                         special_characters=True)
+graph = pydotplus.graph_from_dot_data(dot_data)
+graph.write_pdf("iris.pdf")
+
+
+
+
+
+
+
+
+
+
 
 
 
